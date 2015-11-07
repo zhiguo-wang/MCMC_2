5db34b17086b8e7fb58e32c4c9013dfe45409e83
zhiguo-wang <jeremy.zw@aol.com>
2015-11-07 14:31:55 -0500

    config file

---
+++ Config.R
@@ -0,0 +1,12 @@
+root <- "D:\\OneDrive\\Documents\\MCMC_2\\"
+sourceCode <- "sourceCode\\R Model\\" 
+workspaces <- "Workspaces\\"
+setwd(root)
+
+
+
+
+root <- "C:\\Users\\zhiguo\\OneDrive\\Documents\\MCMC_2\\"
+sourceCode <- "sourceCode\\R Model\\" 
+workspaces <- "Workspaces\\"
+setwd(root)
