a083a7b264b67e17881c86b251d2ed6315daf15b
zhiguo-wang <jeremy.zw@aol.com>
2015-12-06 23:59:12 -0500

    retirement needs = regression calculation

--- Pre_CashFlow_ExpensesForDisability&Death&Retirement.R
+++ Pre_CashFlow_ExpensesForDisability&Death&Retirement.R
@@ -77,20 +77,36 @@ cashOut_Death <- -(pre_mat_income * repSurvivorIncome + burialCost) * mat.death
 cashOut_RetirementNeeds <- matrix(0, nrow(pre_mcmc), ncol(pre_mcmc))
 
 ## income matrix for post
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+# 3.2.2 Cash Outflow_LTC Living Expenses
 
-post_vec_income <- annualIncome * (1 + wageInflation) ^ ((preYears+1) : nrow(mcmc))
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+retireSalary <- annualIncome * (1 + wageInflation) ^ (retireAge - currentAge)
+a0 <- 5313.678641
+a1 <- 0.179338079
 
-post_mat_income <- replicate(ncol(post_mcmc), post_vec_income)
+b0 <- -780.2190476
+b1 <- 80.20857143
 
-post_mat_income[nrow(post_mcmc), ] <- 0
+c0 <- 1311.347392
+c1 <- -120.3701846
+c2 <- 0.288165909
 
-## Assumption of Retirement Financial Needs: Substitution Rate of Wage = 0.7
-## Assumption of LTC Needs: Substitution Rate of Wage = 0.7 * 1.5 = 1.05
-post_cashOut_RetirementNeeds <- -post_vec_income * 0.75 * ((post_mcmc == 1) + (post_mcmc == 2) * 2)
-post_cashOut_RetirementNeeds <- (post_cashOut_RetirementNeeds + annualSSB) * (post_mcmc != 3)
+cashout.mat.retire <- 0 * post_mcmc
+cashout.mat.retire[which(post_mcmc == 1)] <- (a0 + a1 * retireSalary)+ (b0 + b1 * retireAge) + 
+                                         (c0 + c1 * retireAge + c2 * retireSalary)
+
+cashout.mat.retire[which(post_mcmc == 2)] <- ((a0 + a1 * retireSalary)+ (b0 + b1 * retireAge) + 
+                                         (c0 + c1 * retireAge + c2 * retireSalary)) * 2
+
+cashout.mat.retire <- cashout.mat.retire * as.vector((1 + livExpInflation) ^ (1 : nrow(post_mcmc)-1))
+
+cashout.mat.retireNeeds <- (cashout.mat.retire - annualSSB) * (post_mcmc != 3)
 
 generalDiscountFactor <- (1 + generalInflation) ^ -(1:nrow(post_mcmc))
-vec_cashOut_RetirementNeeds <- apply(generalDiscountFactor * post_cashOut_RetirementNeeds, 2, sum)
+vec_cashOut_RetirementNeeds <- -apply(generalDiscountFactor * cashout.mat.retireNeeds, 2, sum)
+apv.retirementNees <- mean(vec_cashOut_RetirementNeeds[vec_cashOut_RetirementNeeds != 0])
 
-cashOut_RetirementNeeds[nrow(pre_mcmc), ] <- vec_cashOut_RetirementNeeds
+
+cashOut_RetirementNeeds[nrow(pre_mcmc), ] <- apv.retirementNees
